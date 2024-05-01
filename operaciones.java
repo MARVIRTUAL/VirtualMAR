f448faf0b81e2cadabd782bf3e2fb5115131f48f
majo_zat <marijo.zat@hotmail.com>
2024-05-01 16:15:38 -0700

    En esta version del proyecto agregue dos metodos de suma y multiplicacion

---
+++ operaciones.java
@@ -0,0 +1,35 @@
+ public class operaciones{
+
+private int suma;
+private int mult;
+private int resta;
+private int numero 1,numero 2;
+
+
+operaciones (int num1,int num2){
+
+    this.numero1=num1
+    this.numero2=num2
+
+    }
+    
+    
+    public void sumaDdosnum(int nume1, int nume2){
+        
+
+     suma= nume1+nume2;
+
+     system.out.println("La suma de los dos numero es : "+suma);
+
+
+    }
+
+    public void multDdosnum(int nume1, int nume2){
+        
+
+        suma= nume1*nume2;
+   
+        system.out.println("La multiplicacion de los dos numero es : "+mult);
+
+         
+    }
