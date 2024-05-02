b829e2f970a5dba8c095ab1fb362867b637b2bb9
majo_zat <marijo.zat@hotmail.com>
2024-05-01 17:05:11 -0700

    termine mi programa

---
+++ principal.java
@@ -0,0 +1,14 @@
+public class principal {
+   public static void main (String[] args) {
+   operaciones opera = new operaciones();
+   
+    opera.sumaDdosnum (nume1:5,nume2:5);
+    opera.multtDdosnum(nume1:8,nume2:3)
+
+   }
+ 
+   
+
+   
+ 
+}
